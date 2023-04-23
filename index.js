8184cd402b6385d9df8810af9a4355cf2fcad22c
jmcavaddy <jakemcavaddy@gmail.com>
2023-04-23 17:21:36 -0400

    Added seeds and starting schema; added connection to database; added inquirer prompts

---
+++ index.js
@@ -0,0 +1,41 @@
+const db = require('./scripts/connection.js');
+const promptUser = require('./scripts/prompt.js');
+
+// Function to start application
+const startApp = async () => {
+    // Prompt user to select an action
+    const { action } = await promptUser();
+
+    // Switch statement to handle user selection
+    switch (action) {
+        case 'View all departments':
+            viewDepartments();
+            break;
+        case 'View all roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+        case 'Exit':
+            db.end();
+            break;
+        default:
+            console.log(`Invalid action: ${action}`);
+            break;
+    }
+};
+
+startApp();
